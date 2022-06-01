e802d679c72c729facec30a9e895ba01f2f61301
Kong32082020 <k18653110045@126.com>
2022-06-01 18:10:07 +0800

    FIx.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const music_name = ["稻香","夏天的风","爱你","一人行者"];
 function musicdown(obj)
 {
     var mname = obj.innerText;
@@ -10,7 +11,37 @@ function init_no()
         nos[i].innerText = i+1;
     }
 }
+function init_music()
+{
+    for(var i=0;i<music_name.length;i++)
+    {
+        var l = document.getElementsByClassName("music-list");
+        var di = document.createElement("div");
+        di.setAttribute("class","music-item");
+        var dino = document.createElement("span");
+        dino.setAttribute("class","music-item-no");
+        var dina = document.createElement("dina");
+        dina.setAttribute("class","music-item-name");
+        dina.setAttribute("onclick","musicdown(this)");
+        dina.innerText = music_name[i];
+        di.appendChild(dino);
+        di.appendChild(dina);
+        l[0].appendChild(di);
+    }
+}
 function init()
 {
+    init_music();
     init_no();
 }
+var cloneObj = function (obj) {
+    var newObj = {};
+    if (obj instanceof Array) {
+      newObj = [];
+    }
+    for (var key in obj) {
+      var val = obj[key];
+      newObj[key] = typeof val === 'object' ? cloneObj(val) : val;
+    }
+    return newObj;
+};
