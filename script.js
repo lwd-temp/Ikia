f7d2bbca9d2af224ea5b5a79de1bd8981f64e945
Kong32082020 <k18653110045@126.com>
2022-06-02 16:12:53 +0800

    Music preview function added.

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const records_name = ["第二次测试直播"];
 const records_link = [
     "https://storage.wildfire.vtb.link/d/%E9%87%8E%E7%81%AB%E5%AD%98%E6%A1%A3/%E5%BD%95%E6%92%AD/%E9%87%8E%E7%81%AB%E4%BC%81%E5%88%92%E6%B5%8B%E8%AF%95%E7%9B%B4%E6%92%AD.mp4"
 ];
+
 //作者名称，作者uid，图片b站链接，图片日期
 const images = [
     ["反野火吧主",321874439,"666438985751461890","2022-5-31"],
     ["烛庇",6578211,"665942448897261623","2022-5-30"]
 ];
+var now_selected=0
+var now_playing = false;
 function changeimg(obj){
     var c = Number(obj.innerText) - 1
     document.getElementById("author").innerText=images[c][0];
@@ -15,10 +18,9 @@ function changeimg(obj){
     document.getElementById("2_img").setAttribute("src","./Images/"+(c+1)+".webp");
     document.getElementById("2_img").setAttribute("onclick","window.open('http://t.bilibili.com/" + images[c][2] + "')");
 }
-function musicdown(obj)
+function musicdown(t)
 {
-    var mname = obj.innerText;
-    window.open("./Musics/" + mname + ".flac");
+    window.open("./Musics/" + t + ".flac");
 }
 function recorddown(obj)
 {
@@ -44,7 +46,7 @@ function init_music()
         dino.innerText = i+1
         var dina = document.createElement("dina");
         dina.setAttribute("class","item-name");
-        dina.setAttribute("onclick","musicdown(this)");
+        dina.setAttribute("onclick","select(this)");
         dina.innerText = music_name[i];
         di.appendChild(dino);
         di.appendChild(dina);
@@ -88,14 +90,33 @@ function init()
     init_records();
     init_images();
 }
-var cloneObj = function (obj) {
-    var newObj = {};
-    if (obj instanceof Array) {
-      newObj = [];
+function select(obj){
+    now_selected=obj.innerText;
+    var a = document.getElementById("music-list");
+    for(var i=0;i<a.children.length;i++)
+    {
+        a.children[i].children[1].style.backgroundColor = "#2D3748";
     }
-    for (var key in obj) {
-      var val = obj[key];
-      newObj[key] = typeof val === 'object' ? cloneObj(val) : val;
+    obj.style.backgroundColor = "#435169";
+}
+function musicplay(t){
+    var ma = document.getElementById("m-audio");
+    if(now_selected!=0){
+        ma.setAttribute("src","./Musics/" + t + ".flac");
+        ma.play();
+        now_playing=true;
+        document.getElementById("play-button").setAttribute("src","./Icons/pause.png");
     }
-    return newObj;
-};
+}
+function musicstop(){
+    var ma = document.getElementById("m-audio");
+    ma.pause();
+    now_playing=false;
+    document.getElementById("play-button").setAttribute("src","./Icons/play.png");
+}
+function music2p(t){
+    if(now_playing)musicstop();
+    else{
+        musicplay(t);
+    }
+}
